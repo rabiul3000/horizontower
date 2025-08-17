2263d1ac8261bb3b73dad94615cc61f888fe951a
rabiul3000 <mdrabiul.asia@gmail.com>
2025-08-17 10:20:35 +0600

    added extra sections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import AboutBuilding from "../../components/AboutBuilding/AboutBuilding";
+import Amenities from "../../components/Amenities/Amenities";
 import Banners from "../../components/Banners/Banners";
 import Coupons from "../../components/Coupons/Coupons";
 import Footer from "../../components/Footer/Footer";
 import Location from "../../components/Location/Location";
+import NewsLatter from "../../components/NewsLatter/NewsLatter";
+import SalesPromotion from "../../components/SalesPromotion/SalesPromotion";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 const Home = () => {
   return (
@@ -10,6 +14,10 @@ const Home = () => {
       <Banners />
       <AboutBuilding />
       <Coupons />
+      <Amenities />
+      <SalesPromotion />
+      <Testimonials />
+      <NewsLatter />
       <Location />
       <Footer />
     </div>
